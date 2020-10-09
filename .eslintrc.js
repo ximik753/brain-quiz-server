2401260a6022a2fba35f18bd7b155d9089d083ca
ximik_753 <sankovdiman@mail.ru>
2020-10-09 15:52:06 +0300

    init

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    env: {
+        browser: true,
+        commonjs: true,
+        es2021: true
+    },
+    extends: [
+        'standard'
+    ],
+    parserOptions: {
+        ecmaVersion: 12
+    },
+    rules: {
+        semi: 'error',
+        indent: ['error', 4]
+    }
+}
