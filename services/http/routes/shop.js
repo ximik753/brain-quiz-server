94b7ba099332272598daedb5b6a83db1f80c21a9
ximik_753 <sankovdiman@mail.ru>
2020-10-16 18:33:30 +0300

    The correct response, after buying a booster

--- services/http/routes/shop.js
+++ services/http/routes/shop.js
@@ -15,6 +15,19 @@ function transformBoosters (boosters) {
     })
 }
 
+function transformBuyData (data) {
+    data.boosters = data.boosters.map(booster => {
+        booster.booster = {
+            title: booster.booster._doc.title,
+            icon: `https://brain-quiz-server.herokuapp.com/images/${booster.booster._doc.icon}.png`
+        }
+
+        return booster
+    })
+
+    return data
+}
+
 router.get('/', auth, async (req, res) => {
     let boosters = await Booster.find({ enabled: true }, { __v: false })
     boosters = transformBoosters(boosters)
@@ -27,12 +40,14 @@ router.get('/', auth, async (req, res) => {
 router.post('/:id', auth, async (req, res) => {
     try {
         const booster = await Booster.findById(req.params.id)
-        const data = await req.user.buyBooster(booster)
+        let data = await req.user.buyBooster(booster)
 
-        if (typeof data === 'object')
+        if (typeof data === 'object') {
+            data = transformBuyData(data)
             res.json({ response: { ...data } })
-        else
+        } else {
             res.status(400).json({ error: data })
+        }
     } catch (e) {
         res.status(400).json({ error: 'Бустер не найден' })
     }
