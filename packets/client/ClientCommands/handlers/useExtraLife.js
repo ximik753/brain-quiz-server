b3e0709d5c9cbe65dfd472038bb0ea108a352857
ximik_753 <sankovdiman@mail.ru>
2020-10-30 15:30:26 +0300

    fix bug with using extra life

--- packets/client/ClientCommands/handlers/useExtraLife.js
+++ packets/client/ClientCommands/handlers/useExtraLife.js
@@ -1,8 +1,10 @@
 const extraLifeBooster = '5f84053fe483452d447c8d2f'
 
 module.exports = async session => {
-    console.log(session.game.incorrectAnswer, game.currentQuestionNumber)
-    if (!session.game.useExtraLife && session.game.incorrectAnswer === game.currentQuestionNumber) {
+    if (!session.game.useExtraLife &&
+        session.game.incorrectAnswer === game.currentQuestionNumber &&
+        game.currentQuestionNumber !== game._totalQuestions
+    ) {
         const booster = session.user.boosters.find(item => item.booster.toString() === extraLifeBooster)
 
         if (booster) {
